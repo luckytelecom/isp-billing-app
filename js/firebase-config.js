c407a8a41ec905bd39b06ad250eaf77771bcc1a8
luckytelecom <luckytelecom5276@gmail.com>
2026-01-16 18:45:24 +0600

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,13 +1,12 @@
 // Firebase configuration
 const firebaseConfig = {
-  const firebaseConfig = {
-    apiKey: "AIzaSyA06NK1Y33rmvGHGgyk4isek5GNd7t56is",
-    authDomain: "isp-billing-app-a4ce9.firebaseapp.com",
-    projectId: "isp-billing-app-a4ce9",
-    storageBucket: "isp-billing-app-a4ce9.firebasestorage.app",
-    messagingSenderId: "331467133325",
-    appId: "1:331467133325:web:289aabb566b27358bd2dc4",
-    measurementId: "G-5CPGLRJ8QK"
+  apiKey: "AIzaSyA06NK1Y33rmvGHGgyk4isek5GNd7t56is",
+  authDomain: "isp-billing-app-a4ce9.firebaseapp.com",
+  projectId: "isp-billing-app-a4ce9",
+  storageBucket: "isp-billing-app-a4ce9.firebasestorage.app",
+  messagingSenderId: "331467133325",
+  appId: "1:331467133325:web:289aabb566b27358bd2dc4",
+  measurementId: "G-5CPGLRJ8QK"
 };
 
 // Initialize Firebase
@@ -21,3 +20,4 @@ const db = firebase.firestore();
 window.auth = auth;
 
 window.db = db;
+
